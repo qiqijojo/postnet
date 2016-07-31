9c48fd92bff44a10463312523c4b257db6e232be
wangqi <774948439@qq.com>
2016-08-01 03:18:38 +1100

    router

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const obj = require("./core/converter");
 let currentActionName = 'init';
 
 const actions = [{
@@ -7,15 +7,22 @@ const actions = [{
   help: `
 Welcome
 1 - postcode
-2 - barcode`.trim(),
+2 - barcode
+3 - quit`.trim(),
   doAction(cmd) {
     switch(cmd) {
       case '1':
         currentActionName = 'postcode';
         break;
       case '2':
+        currentActionName = 'barcode';
+            break;
+      case '3':
+        currentActionName = 'quit';
+            break;
       default:
         console.log('Input error\n');
+            break;
     }
   }
 }, {
@@ -23,7 +30,30 @@ Welcome
   help: `
 Please input post code`.trim(),
   doAction(cmd) {
-    console.log('I\'m converting the postcode: ' + cmd);
+    console.log('The postcode is: ' + cmd);
+    console.log('The result of postcode to barcode is:' + obj.transformToBarCode(cmd));
+    currentActionName = "init";
+  }
+},{
+  name:'barcode',
+  help:`
+Please input barcode`.trim(),
+  doAction(cmd){
+    console.log('The barcode is:' + cmd);
+    console.log('The result of barcode to postcode is:' + obj.transformToZipCode(cmd));
+    currentActionName = "init";
+  }
+},{
+  name:'quit',
+  help:`
+Weather you want to exit this interface!(yes/no)`.trim(),
+  doAction(cmd){
+  console.log('Your choice is :' + cmd);
+  if(cmd === "yes"){
+    console.log("退出");
+    process.exit();
+  }
+    currentActionName = "init";
   }
 }];
 
